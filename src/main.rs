df637044bedb6320f42fe61d2a885b5b1a2a9ff0
frezyn <00001097715413SP@al.educacao.sp.gov.br>
2024-05-25 22:17:00 -0300

    feat: Refactor lexer to scan tokens one by one

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,6 @@ struct Args {
 }
 
 
-pub struct Resolver{
-        scope_stack : Vec<HashMap<String, bool>>,
-        pub lex_scope : HashMap<u64, usize>,
-    }
-
 fn main() {
         let args = Args::parse();
         if let Some(a) = args.path {
@@ -33,7 +28,7 @@ fn main() {
 }
 
 fn read_file(path: PathBuf) {
-        let s = fs::read_to_string(path).expect("arquivo nao encontrado");
+        let s = fs::read_to_string(path).expect("Erro ao ler arquivo");
         run(s)
 }
 
@@ -41,10 +36,7 @@ fn run(src: String) {
         let mut scann = Lexer::new(&src);
         scann.scan_tokens();
         let mut p = parse::new(scann.tokens);
-        let smts = p.parse().expect("erro ao parsear estrutura sintatica");
+        let smts = p.parse().expect("erro ao parsear para estrutura sintatica");
         let mut interpreter = Interpreter::new(HashMap::new()); 
         interpreter.inter(smts)
-        // lexer: tokens: [Token Identifier, Token LeftParen, Token Number(Number(2.0)), Token Plus, Token Number(Number(2.0)), Token RightParen, Token TERMINATE], start: 11, current: 12, line: 1, keywords: {"super": Super, "this": This, "or": Or, "let": Let, "nil": Nil, "else": Else, "while": While, "for": For, "return": Return, "struct": Struct, "false": False, "fun": Fun, "class": Class, "break": Break, "continue": Continue, "true": True, "and": And, "loop": Loop, "if": If} } 
-        // Parser { tokens: [Token Identifier, Token LeftParen, Token Number(Number(2.0)), Token Plus, Token Number(Number(2.0)), Token RightParen, Token TERMINATE], current: 0, inloop: false, function_stack: [], s_id: 0 }
-        // [Expression(Call(Primary(Symbol { name: "println" }), Token RightParen, [Binary(Literal(Number(2.0)), Token Plus, Literal(Number(2.0)))]))]
 }
